ff1570259448630c0d10be759346ffa02b5bc6ac
Samuel <samuel.ingemarsson@gmail.com>
2017-11-28 10:25:35 +0100

    Added all predictions and update except Zranger. Improved comments

--- crazyflie-firmware/simulink-model/Matlab/ExtendedKalmanFilter.m
+++ crazyflie-firmware/simulink-model/Matlab/ExtendedKalmanFilter.m
@@ -1,34 +1,26 @@
 
-function [xf, Pf] = ExtendedKalmanFilter(measurements, statePrior, priorCovariance)
+function [xf, Pf] = ExtendedKalmanFilter(measurements)
 %NONLINEARKALMANFILTER Filters measurement sequence Y using a 
 % non-linear Kalman filter. 
-
+% TODO Implement Zranger
 %Input:
-%   Y                       [m x N] Measurement sequence for times 1,...,N
+%   Y                       [m x 1] Measurement sequence for times 1,...,N
 %   statePrior        [n x 1] Prior mean for time 0
 %   priorCovariance    [n x n] Prior covariance
 %   motionModel                     Motion model function handle
 %
 %Output:
-%   xf          [n x N]     Filtered estimates for times 1,...,N
-%   Pf          [n x n x N] Filter error convariance
+%   xf          [n x 1]     Filtered estimates for times 1,...,N
+%   Pf          [n x n ] Filter error convariance
 %
 
-% 
-% N = size(Y,2);
-% n = length(x_0);
-% xf = zeros(n,N);
-% Pf = zeros(n,n,N);
-% xp = zeros(n,N+1);
-% Pp = zeros(n,n,N+1);
 
 
+[xp Pp]=predictionGyro(statePrior, priorCovariance, T,Q_gyro)  %Prediction With Gyro
+[xp Pp]=predictionLPS(xp, Pp, T,R_LPS)                                        %Prediction With LPS
+[xf Pf]=updateAcc(xp, Pp, T,R_acc)                                        % Update With accelerometer
+[xf Pf]=updateLPS(xf, Pf, T,R_LPS)                                        % Update With LPS
 
-[xp Pp]=predictionGyro(statePrior, priorCovariance, samplingTime,processNoise)  %Prediction With Gyro
-[xp Pp]=predictionLPS(xp, Pp, samplingTime,processNoise)                                        %Prediction With LPS
 
-%update ACC
-%update LPS
-%Update Zranger
 end
 
--- crazyflie-firmware/src/modules/interface/rtwtypes.h
+++ crazyflie-firmware/src/modules/interface/rtwtypes.h
@@ -7,9 +7,9 @@
  *
  * Code generated for Simulink model 'crazyflie'.
  *
- * Model version                  : 1.370
+ * Model version                  : 1.374
  * Simulink Coder version         : 8.13 (R2017b) 24-Jul-2017
- * C/C++ source code generated on : Thu Nov 23 16:10:53 2017
+ * C/C++ source code generated on : Thu Nov 23 16:44:45 2017
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex
