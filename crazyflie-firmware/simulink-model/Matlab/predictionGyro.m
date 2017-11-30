d0ba7e234325465ed2a55c6478ac09bf4326a06c
ViktorInsgard <ViktorInsgard@github.com>
2017-11-30 11:15:22 +0100

    Change predictionGyro

--- crazyflie-firmware/simulink-model/Matlab/predictionGyro.m
+++ crazyflie-firmware/simulink-model/Matlab/predictionGyro.m
@@ -10,16 +10,12 @@ function [xp,Pp] = predictionGyro(x, measurement,P, T,Q)
 %   x p          [12 x 1] predicted state mean
 %   Pp           [12 x 12] predicted state covariance
 %
-R_x = [1 0 0; 0 cos(measurement(1)) -sin(measurement(1)); 0 sin(measurement(1)) cos(measurement(1))]; %Rotation matrix, Roll 
-R_y = [cos(measurement(2)) 0 sin(measurement(2)); 0 1 0; -sin(measurement(2)) 0 cos(measurement(2))]; %Rotation matrix, Pitch
-R_z = [cos(measurement(3)) -sin(measurement(3)) 0; sin(measurement(3)) cos(measurement(3)) 0; 0 0 1]; %Rotation matrix, Yaw
-R = R_x*R_y*R_z;
 
-F=[eye(3) T*R; zeros(3) eye(3)];    %Constant velocity
+F=[eye(3) T*eye(3); zeros(3) eye(3)];    %Constant velocity
      
 
 %Kalman prediction
-xtemp=F*x(1:6);
+xtemp=[x(1:3) + T*measurement(1:3); measurement(1:3)];
 xp=[ xtemp; x(7:12)]; 
 Ptemp=F*P(1:6,1:6)*F.'+Q(1:6,1:6);
 P(1:6,1:6)=Ptemp;
